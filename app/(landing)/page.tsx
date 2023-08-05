ddced143e49491b028dedbebe7157f238ebd82dd
ISMAILBOUADDI <ismailbouaddi55@gmail.com>
2023-08-05 12:28:22 +0100

    landing page

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import Link  from "next/link";
+
+const LandingPage = () => {
+    return ( 
+        <>
+            <p className="text-6xl text-orange-600">hello world from ai sass landing</p>
+            <div>
+                <Link href="/sign-in">
+                    <Button>
+                        login
+                    </Button>
+                </Link>
+                <Link href="/sign-up">
+                    <Button>
+                        register
+                    </Button>
+                </Link>
+            </div>
+        </>
+        
+     );
+}
+ 
+export default LandingPage;
