cca7970077ff90f2bf38936f700aed1b42e53ffa
ISMAILBOUADDI <ismailbouaddi55@gmail.com>
2023-08-05 12:27:04 +0100

    styling auth pages

---
+++ app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,11 @@
+const AuthLyout = ({
+    children
+} : { children: React.ReactNode;}) => {
+    return ( 
+        <div className="flex items-center justify-center h-full">
+            {children}
+        </div>
+     );
+}
+ 
+export default AuthLyout
