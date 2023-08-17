8fbadbdf5094dd0c979ab452f546d5281c37bca4
ISMAILBOUADDI <ismailbouaddi55@gmail.com>
2023-08-17 14:56:19 +0100

    up

---
+++ components/mobile-sidbar.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Menu } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Sidbar from "@/components/sidbar";
+
+export const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  return (
+    <>
+    {isMounted && <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidbar/>
+      </SheetContent>
+     </Sheet>}
+     </>
+  );
+};
